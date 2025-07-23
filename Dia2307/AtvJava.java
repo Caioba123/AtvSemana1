3dbaed35c972f45d90f5f2c816f95c71f763304c
Caioba123 <caiomuller7192@gmail.com>
2025-07-23 09:22:42 -0300

    Add files via upload

---
+++ Dia2307/AtvJava.java
@@ -0,0 +1,103 @@
+package Dia2307;
+import java.util.Scanner;
+
+public class AtvJava {
+    public static void main(String[] args) {
+    Scanner tec = new Scanner(System.in);
+    
+    // 1 Questão
+    //double mesa [] = new double[20];
+      
+    //   mesa[0]= 1;
+    //   mesa[1]= 2;
+    //   mesa[2]= 3;
+    //   mesa[3]= 4;
+    //   mesa[4]= 5;
+    //   mesa[5]= 6;
+    //   mesa[6]= 7;
+    //   mesa[7]= 8;
+    //   mesa[8]= 9;
+    //   mesa[9]= 10;
+    //   mesa[10]= 11;
+    //   mesa[11]= 12;
+    //   mesa[12]= 13;
+    //   mesa[13]= 14;
+    //   mesa[14]= 15;
+    //   mesa[15]= 16;
+    //   mesa[16]= 17;
+    //   mesa[17]= 18;
+    //   mesa[18]= 19;
+    //   mesa[19]= 20;
+       
+      // System.out.println("Mesa 1: " +mesa[0]);
+      // System.out.println("Mesa 2: " +mesa[1]);
+      // System.out.println("Mesa 3: " +mesa[2]);
+      // System.out.println("Mesa 4: " +mesa[3]);
+      // System.out.println("Mesa 5: " +mesa[4]);
+      // System.out.println("Mesa 6: " +mesa[5]);
+      // System.out.println("Mesa 7: " +mesa[6]);
+      // System.out.println("Mesa 8: " +mesa[7]);
+      // System.out.println("Mesa 9: " +mesa[8]);
+      // System.out.println("Mesa 10: " +mesa[9]);
+      // System.out.println("Mesa 11: " +mesa[10]);
+      // System.out.println("Mesa 12: " +mesa[11]);
+      // System.out.println("Mesa 13: " +mesa[12]);
+      // System.out.println("Mesa 14: " +mesa[13]);
+      // System.out.println("Mesa 15: " +mesa[14]);
+      // System.out.println("Mesa 16: " +mesa[15]);
+      // System.out.println("Mesa 17: " +mesa[16]);
+      // System.out.println("Mesa 18: " +mesa[17]);
+      // System.out.println("Mesa 19: " +mesa[18]);
+      // System.out.println("Mesa 20: " +mesa[19]);
+      
+      // 2 Questão
+      // int total = 0;
+      // for ( int i = 1; i <= 100; i++) {
+      //    total += i;
+      // }
+      
+      //  System.out.println("Total gasto na compra foi: R$ " +total);
+      
+      //3 Questão
+       
+      // System.out.println("Vamos começar a nossa contagem regressiva!");
+      // System.out.println("Digite o seu nome:"); 
+      // String nome = tec.nextLine();
+      // int total = 10;
+      // for (int i = 10; i >= 1; i--) {
+      // System.out.println(i);
+      // total = i;
+      // }
+      // System.out.println("Feliz 2026");
+      // System.out.println("Feliz Ano Novo, " +nome);
+      
+      //4 Questão
+      
+      //  double[] contagem = new double[10];
+      //  System.out.println("Entre com algum valor:");
+      //  double valor1 = tec.nextDouble();
+      //  contagem[1] = valor1 ;
+      //  int valor2 = (int) (valor1*valor1);
+      //  for (int i = 0 ; i < valor2 ; i++) {
+      //  System.out.println(contagem[1] * i);
+      //  }
+      
+      //5 Questão
+      // System.out.print("Digite um número para calcular o fatorial: ");
+      // int numero = tec.nextInt();
+      // long fatorial = 1;
+      // for (int i = 1; i <= numero; i++) {
+      //      fatorial *= i;
+      // }
+      // System.out.println("Fatorial de " + numero + " é: " + fatorial);
+        
+      //6 Questão
+      //  for (int valor = 1; valor <= 50; valor += 2) {
+      //      System.out.println(valor);  
+      //  }
+
+      //7 Questão
+      
+      //8 Questão
+}
+}
